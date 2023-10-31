37b813fab630cb2b097cbd7f25b727bc89f0b6b5
rawrex <rawrex@proton.me>
2023-10-31 14:07:13 +0700

    Added the clarification comments.

--- chapter_1/exercise_1.9.rkt
+++ chapter_1/exercise_1.9.rkt
@@ -16,5 +16,15 @@
       b
       (sum-iter (decrement a) (increment b))))
 
+;; The recursive sum...
 (sum-recursive 4 5)
+;; ...will have the following form
+;; (increment (increment (increment (increment 5)))) ;; -> 9
+
+;; The iterative sum...
 (sum-iter 4 5)
+;; ...will have the following form
+;; (sum-iter 3 6)
+;; (sum-iter 2 7)
+;; (sum-iter 1 8)
+;; (sum-iter 0 9) ;; -> 9
