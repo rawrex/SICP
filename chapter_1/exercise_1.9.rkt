d9435e3c83f69b2813b95354edf5b6693d3cb2b7
rawrex <rawrex@proton.me>
2023-10-31 13:59:41 +0700

    Added the exercise part from the book.

--- chapter_1/exercise_1.9.rkt
+++ chapter_1/exercise_1.9.rkt
@@ -1,1 +1,20 @@
 #lang sicp
+
+(define (increment x)
+  (+ x 1))
+
+(define (decrement x)
+  (- x 1))
+
+(define (sum-recursive a b)
+  (if (= a 0)
+      b
+      (increment (sum-recursive (decrement a) b))))
+
+(define (sum-iter a b)
+  (if (= a 0)
+      b
+      (sum-iter (decrement a) (increment b))))
+
+(sum-recursive 4 5)
+(sum-iter 4 5)
