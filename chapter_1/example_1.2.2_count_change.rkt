a8add0addc35e6c58f9534eaf8509de2ffd313e9
rawrex <rawrex@proton.me>
2023-11-06 17:10:35 +0700

    Changed the denominations and call examples.

--- chapter_1/example_1.2.2_count_change.rkt
+++ chapter_1/example_1.2.2_count_change.rkt
@@ -12,12 +12,12 @@
 
 (define (first-denomination kinds-of-coins)
   (cond ((= kinds-of-coins 1) 1)
-        ((= kinds-of-coins 2) 5)
-        ((= kinds-of-coins 3) 10)
-        ((= kinds-of-coins 4) 25)
+        ((= kinds-of-coins 2) 2)
+        ((= kinds-of-coins 3) 5)
+        ((= kinds-of-coins 4) 10)
         ((= kinds-of-coins 5) 50)))
 
 (count-change 1)
 (count-change 10)
 (count-change 100)
-(count-change 1000)
+(count-change 500)
