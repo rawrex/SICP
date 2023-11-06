46fc941622d22d848d83dda63b5d369577eeb753
rawrex <rawrex@proton.me>
2023-11-07 02:04:48 +0700

    Added the prints to show how the invariant holds throughout the computation.

--- chapter_1/exercise_1.16.rkt
+++ chapter_1/exercise_1.16.rkt
@@ -7,11 +7,21 @@
         ((even? n) (square (fast-exponent-recursive b (/ n 2))))
         (else (* b (fast-exponent-recursive b (- n 1))))))
 
+(define (print-invariant b n accumulator)
+  (display "b: ")(display b)(newline)
+  (display "n: ")(display n)(newline)
+  (display "a: ")(display accumulator)(newline)
+  (display "a*b^n: ")(display (* accumulator (expt b n)))(newline)(newline))
+
 (define (fast-exponent-iterative b n)
   (define (iter b n accumulator)
     (cond ((= n 0) accumulator)
-          ((even? n) (iter (square b) (/ n 2) accumulator))
-          (else (iter b (- n 1) (* b accumulator)))))
+          ((even? n)
+           (print-invariant b n accumulator)
+           (iter (square b) (/ n 2) accumulator))
+          (else
+           (print-invariant b n accumulator)
+           (iter b (- n 1) (* b accumulator)))))
   (iter b n 1))
 
 (fast-exponent-recursive 2 32)
