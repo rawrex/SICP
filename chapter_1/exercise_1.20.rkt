8ec37f94a5b4423314b223f5b3db08298a86d31a
rawrex <rawrex@proton.me>
2023-11-16 01:59:43 +0700

    Copied the contents of the example into the exercise file.

--- chapter_1/exercise_1.20.rkt
+++ chapter_1/exercise_1.20.rkt
@@ -1,2 +1,13 @@
 #lang sicp
 
+; Iterative implementation of Euclid's Algorithm for finding GCD
+
+(define (GCD a b)
+  (if (= b 0)
+      a
+      (GCD b (remainder a b))))
+
+; Tests
+(GCD 28 16)
+(GCD 120 45)
+(GCD 206 40)
