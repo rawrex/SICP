040e931f5073766fbd81d87766d43295cff7204a
rawrex <rawrex@proton.me>
2023-11-16 14:46:25 +0700

    Added a halfhearted solution to the exercise 1.20.

--- chapter_1/exercise_1.20.rkt
+++ chapter_1/exercise_1.20.rkt
@@ -2,10 +2,48 @@
 
 ; Iterative implementation of Euclid's Algorithm for finding GCD
 
+(define R remainder)
+
 (define (GCD a b)
   (if (= b 0)
       a
-      (GCD b (remainder a b))))
+      (GCD b (R a b))))
+
+; Normal order
+;
+; (GCD 206 40)
+;
+; (GCD 40 (R 206 40))
+;  -> when inside GCD, executes R in the predicate -> 6
+;
+; (GCD
+;      (R 206 40)
+;      (R 40 (R 206 40))
+; -> when inside GCD, executes R in the predicate -> 4
+;
+; (GCD
+;      (R 40 (R 206 40))
+;      (R (R 206 40) (R 40 (R 206 40))))
+; -> when inside GCD, executes R in the predicate -> 2
+;
+; (GCD
+;      (R (R 206 40) (R 40 (R 206 40)))
+;      (R (R 40 (R 206 40)) (R (R 206 40) (R 40 (R 206 40))))))
+; -> when inside GCD, executes R in the predicate -> 0
+;
+; returns 2
+; R was called 18 times (11 in GCD calls + 3 in predicates + 4 in final predicate) (???)
+
+; Applicative order
+;
+; (GCD 206 40)
+; (GCD 40 6) -> before GCD, executes (R 206 40) -> 6
+; (GCD 6 4) -> before GCD, executes (R 40 6) -> 4
+; (GCD 4 2) -> before GCD, executes (R 6 4) -> 2
+; (GCD 2 0) -> before GCD, executes (R 4 2) -> 0
+; returns 2
+;
+; R was called four times
 
 ; Tests
 (GCD 28 16)
