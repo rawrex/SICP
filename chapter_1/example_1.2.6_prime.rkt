5deecc8c224369f9ed2b9bdcdd76e4d243bbba6a
rawrex <rawrex@proton.me>
2024-01-25 15:20:41 +0700

    Added the example code for the smallest division testing.

--- chapter_1/example_1.2.6_prime.rkt
+++ chapter_1/example_1.2.6_prime.rkt
@@ -1,2 +1,19 @@
 #lang sicp
 
+(define (smallest-divisor n) (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond
+    ;; if we take test-divisor test-divisor times,
+    ;; then, if we are going in ascending order,
+    ;; test-divisor should already been shown to be a divisor of n
+    ((> (square test-divisor) n) n)
+    ((divides? test-divisor n) test-divisor)
+    (else (find-divisor n (inc test-divisor)))))
+
+(define (square x)
+  (* x x))
+
+(define (divides? divisor number)
+  (= (remainder number divisor) 0))
+        
