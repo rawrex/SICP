2510216c92099d64d99fa63a8f4da52e6b556abb
rawrex <rawrex@proton.me>
2024-01-28 21:52:42 +0700

    Done the example

--- chapter_1/example_1.2.6_prime.rkt
+++ chapter_1/example_1.2.6_prime.rkt
@@ -20,5 +20,27 @@
 (define (divides? divisor number)
   (= (remainder number divisor) 0))
 
-(define (prime? n)
+(define (slow-prime? n)
   (= (smallest-divisor n) n))
+
+
+;; The Fermat Test
+
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp) (remainder (square (expmod base (/ exp 2) m)) m))
+        (else (remainder (* base (expmod base (- exp 1) m)) m))))
+
+(define (fermat-test n)
+  (define (try-it a)
+    (= (expmod a n n) a))
+  (try-it (+ 1 (random (- n 1)))))
+
+(define (fast-prime? n times)
+  (cond ((= times 0) true)
+        ((fermat-test n) (fast-prime? n (- times 1)))
+        (else false)))
+
+(define (prime? n)
+  (fast-prime? n 3))
+
