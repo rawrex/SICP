d1293b78c07a19317713fb854deaca1f9a52a7e7
rawrex <rawrex@proton.me>
2024-01-29 14:11:43 +0700

    Initial commit for the ex 1.23

---
+++ chapter_1/exercise_1.23.rkt
@@ -0,0 +1,18 @@
+#lang sicp
+
+(define (prime-a? n)
+  (= (smallest-divisor n) n))
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (inc test-divisor)))))
+
+(define (divides? divisor number)
+  (= (remainder number divisor) 0))
+
+(define (square n)
+  (* n n))
