625365b312f786a8ea55b23e091ece28c80942cb
rawrex <rawrex@proton.me>
2024-02-01 19:02:10 +0700

    Removed the timed tests with the Fermat algorithm, since they are going to be considered in the exercise 1.24.

--- chapter_1/exercise_1.22.rkt
+++ chapter_1/exercise_1.22.rkt
@@ -5,7 +5,7 @@
 
 ;; Smallest Divisor Test
 
-(define (prime-a? n)
+(define (prime-GCD? n)
   (define (divides? divisor number)
     (= (remainder number divisor) 0))
   (define (find-divisor n test-divisor)
@@ -16,24 +16,6 @@
   (define (smallest-divisor n)
     (find-divisor n 2))
   (= (smallest-divisor n) n))
-
-
-;; The Fermat Test
-
-(define (prime-b? n)
-  (define (expmod base exp m)
-    (cond ((= exp 0) 1)
-          ((even? exp) (remainder (square (expmod base (/ exp 2) m)) m))
-          (else (remainder (* base (expmod base (- exp 1) m)) m))))
-  (define (fermat-test n)
-    (define (try-it a)
-      (= (expmod a n n) a))
-    (try-it (+ 1 (random (- n 1)))))
-  (define (fast-prime? n times)
-    (cond ((= times 0) true)
-          ((fermat-test n) (fast-prime? n (- times 1)))
-          (else false)))
-  (fast-prime? n 3))
 
 
 ;; The Profiling function
@@ -61,7 +43,7 @@
   (cond ((and (< begin end) (even? begin))
          (find-primes (inc begin) end))
         ((< begin end)
-         (timed-prime-test begin prime-b?)
+         (timed-prime-test begin prime-GCD?)
          (find-primes (+ 2 begin) end))))
 
 
@@ -74,10 +56,6 @@
 
 (define (run-tests)
   (display "GCD test:")
-  (find-n-primes 100000 3 prime-a?)     ; average time: 50 
-  (find-n-primes 1000000 3 prime-a?)    ; average time: 150 
-  (find-n-primes 10000000 3 prime-a?)   ; average time: 385 
-  (display "\n* * *\nFermat test:")
-  (find-n-primes 10000000 3 prime-b?)   ; average time: 20 
-  (find-n-primes 100000000 3 prime-b?)  ; average time: 23 
-  (find-n-primes 1000000000 3 prime-b?)); average time: 25 
+  (find-n-primes 100000 3 prime-GCD?)     ; average time: 50 
+  (find-n-primes 1000000 3 prime-GCD?)    ; average time: 150 
+  (find-n-primes 10000000 3 prime-GCD?))  ; average time: 385
