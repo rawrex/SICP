11a62255f2ea75652b7800d0662d5d29a29bc0db
rawrex <rawrex@proton.me>
2024-02-15 00:02:01 +0700

    Added the triviality test.

--- chapter_1/exercise_1.28.rkt
+++ chapter_1/exercise_1.28.rkt
@@ -23,6 +23,13 @@
   (cond ((= exp 0) 1)
         ((even? exp) (remainder (square (expmod base (/ exp 2) m)) m))
         (else (remainder (* base (expmod base (- exp 1) m)) m))))
+
+(define (is-nontrivial-sqrt x n)
+  (if (and
+       (or (not (= 1 x)) (not (= x (- n 1))))
+       (= (remainder (square x) n) 1))
+      0
+      1))
   
 (define (square x)
   (* x x))
