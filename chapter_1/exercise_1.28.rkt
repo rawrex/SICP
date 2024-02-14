cffa2f48ffc6722033990099110ccbe392349193
rawrex <rawrex@proton.me>
2024-02-14 23:45:19 +0700

    Repositioned the definitions and added the modified test.

--- chapter_1/exercise_1.28.rkt
+++ chapter_1/exercise_1.28.rkt
@@ -1,19 +1,28 @@
 #lang sicp
 
 (define (prime? n)
-  (define (fast-prime? n times)
-    (cond ((= times 0) #t)
-          ((fermat-test n) (fast-prime? n (- times 1)))
-          (else #f)))
-  (define (fermat-test n)
-    (try-it (+ 1 (random (- n 1)))))
-  (define (try-it a)
-    (= (new-expmod a (- n 1) n) a))
   (fast-prime? n 3))
 
-(define (new-expmod base exp m)
+(define (fast-prime? n times)
+  (cond ((= times 0) #t)
+        ;((fermat-test n) (fast-prime? n (- times 1)))
+        ((miller-rabin-test n) (fast-prime? n (- times 1)))
+        (else #f)))
+
+(define (miller-rabin-test n)
+  (define (try-it a)
+    (= (expmod a (- n 1) n) 1))
+  (try-it (+ 1 (random (- n 1)))))
+
+(define (fermat-test n)
+  (define (try-it a)
+    (= (expmod a (- n 1) n) a))
+  (try-it (+ 1 (random (- n 1)))))
+
+(define (expmod base exp m)
   (cond ((= exp 0) 1)
-        ((even? exp) (remainder (square (new-expmod base (/ exp 2) m)) m))
-        (else (remainder (* base (new-expmod base (- exp 1) m)) m))))
-        
+        ((even? exp) (remainder (square (expmod base (/ exp 2) m)) m))
+        (else (remainder (* base (expmod base (- exp 1) m)) m))))
   
+(define (square x)
+  (* x x))
