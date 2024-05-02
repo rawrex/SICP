f80692a0e261b67576e8c4a85e49fbaf61b971f1
rawrex <rawrex@proton.me>
2024-05-02 19:27:16 +0700

    Added a faulty outline for the solution.

--- chapter_1/exercise_1.29.rkt
+++ chapter_1/exercise_1.29.rkt
@@ -5,4 +5,22 @@
       0
       (+ (term a) (sum term (next a) next b))))
 
+(define (old-integral f a b dx)
+  (define (add-dx x)
+    (+ x dx))
+  (* (sum f (+ a (/ dx 2.0)) add-dx b)
+     dx))
 
+(define accuracy 3)
+
+(define (new-integral f a b n)
+  (define (h a b n)
+    (/ (- b a) n))
+  (define (y_k k)
+    (f (+ a (* k (h a b n)))))
+  (define (foo k)
+    (* (sum f a (y_k (inc k)) b)
+       (/ (h a b n) 3.0)))
+  (foo 0.0))
+
+(new-integral inc 0 10 accuracy)
