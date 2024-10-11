76dc644f7242f064b6c50410399179dde1132c43
rawrex <rawrex@proton.me>
2024-10-11 20:11:31 +0700

    Corrected the implementation.

--- chapter_1/exercise_1.29.rkt
+++ chapter_1/exercise_1.29.rkt
@@ -5,25 +5,17 @@
       0
       (+ (term a) (sum term (next a) next b))))
 
-(define (old-integral f a b dx)
-  (define (add-dx x)
-    (+ x dx))
-  (* (sum f (+ a (/ dx 2.0)) add-dx b)
-     dx))
-
-(define (self x ) x)
-
 (define (new-integral f a b n)
+  ;; Subinterval width
   (define h (/ (- b a) n))
-  (define (y_k k)
-    (f (+ a (* k h))))
-  (* (/ h 3.0)
-     (+
-        (y_k 0)
-        (* 4 (sum self (y_k (dec n)) ...))
-        (* 2 (sum self (y_k (dec n)) ...))
-        (y_k n))))
-
+  ;; The rule for each step
+  ;; Note the weights 1, 2, 4
+  (define (term x)
+    (+ (f x) (* 4 (f (+ x h))) (f (+ x (* 2 h)))))
+  ;; Advances the current point
+  (define (next x)
+    (+ x (* 2 h)))
+  (* (/ h 3.0) (sum term a next (- b (* 2 h)))))
 
 (define (cube x)
   (* x x x))
