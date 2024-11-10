a98417c8395b82e957fcbd350903214822d496cd
rawrex <rawrex@proton.me>
2024-11-10 14:27:59 +0700

    Added a solution for the exercese 1.43.

--- chapter_1/exercise_1.43.rkt
+++ chapter_1/exercise_1.43.rkt
@@ -6,6 +6,7 @@
 (define (repeat f n)
   (if (= n 1)
     (lambda (x) (f x))
-    (compose f (repeat f (dec n)))))
+    ; (compose f (repeat f (dec n)))))
+    (lambda (x) (f ((repeat f (dec n)) x)))))
 
 ((repeat (lambda (x) (* x x)) 2) 5)
