b23765cb5c6184877a19735c9c50064870bc7e97
rawrex <rawrex@proton.me>
2024-11-22 14:13:53 +0700

    Added a definition for the insertion sort from the Berkeley lecture.

---
+++ chapter_1/cs61a_lecture_6.rkt
@@ -0,0 +1,14 @@
+#lang sicp
+
+(define (insertion-sort input-numbers)
+  (define (insert number sorted-numbers)
+    (cond ((null? sorted-numbers) (list number))
+          ((< number (car sorted-numbers)) (cons number sorted-numbers))
+          ((cons (car sorted-numbers) (insert number (cdr sorted-numbers))))))
+  (if (null? input-numbers)
+      '()
+      (insert (car input-numbers)
+              (insertion-sort (cdr input-numbers)))))
+
+(define test (list 2 -1 3 1 5 0))
+(insertion-sort test)
