0debc74a6e214932451399589575f7ceca24e18e
rawrex <rawrex@proton.me>
2024-12-10 21:08:21 +0700

    Added constructors, selectors and basic operations for the exercise 2.3.

---
+++ chapter_2/exercise_2.3.scm
@@ -0,0 +1,92 @@
+#lang sicp
+
+;; Common
+(define (square x)
+  (* x x))
+
+;; Point
+(define (make-point x y)
+  (cons x y))
+
+(define (point-x point)
+  (car point))
+
+(define (point-y point)
+  (cdr point))
+
+
+;; Segment
+(define (make-segment start-point end-point)
+  (cons start-point end-point))
+
+(define (segment-start segment)
+  (car segment))
+
+(define (segment-end segment)
+  (cdr segment))
+
+(define (segment-length segment)
+  (sqrt (+
+         (+ (square (point-x (segment-start segment)))
+            (square (point-x (segment-end segment))))
+         (+ (square (point-y (segment-start segment)))
+            (square (point-y (segment-end segment)))))))
+
+
+;; Rectangle
+;;      a
+;;   A-----B
+;; d |     | b
+;;   D-----C
+;;      c
+
+;; Constructor(s)
+(define (make-rect point-A point-C)
+  (cons (point-A point-C)))
+
+;; Selectors for vertices
+(define (rect-A rect)
+  (car rect))
+(define (rect-B rect)
+  (make-point (point-x (rect-C rect)) (point-y (rect-A rect))))
+(define (rect-C rect)
+  (cdr rect))
+(define (rect-D rect)
+  (make-point (point-x (rect-A rect)) (point-y (rect-C rect))))
+
+;; Selectors for sides
+(define (rect-a rect)
+  (make-segment (rect-A rect) (rect-B rect)))
+(define (rect-b rect)
+  (make-segment (rect-B rect) (rect-C rect)))
+(define (rect-c rect)
+  (make-segment (rect-C rect) (rect-D rect)))
+(define (rect-d rect)
+  (make-segment (rect-D rect) (rect-A rect)))
+
+
+;; Operations on the rectangles
+(define (rect-perimiter rect)
+  (+
+   (segment-length (rect-a rect))
+   (segment-length (rect-b rect))
+   (segment-length (rect-c rect))
+   (segment-length (rect-d rect))))
+
+(define (rect-area rect)
+  (*
+   (segment-length (rect-a rect))
+   (segment-length (rect-b rect))))
+  
+
+;; Utils
+(define (print-point p)
+  (newline)
+  (display "(")
+  (display (point-x p))
+  (display ", ")
+  (display (point-y p))
+  (display ")"))
+
+
+;; Main
