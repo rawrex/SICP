d1e8b11a28844856a4990b717ded02cbf123bcd0
rawrex <rawrex@proton.me>
2024-12-24 21:57:50 +0700

    Added a solution for the ex 2.4

---
+++ chapter_2/exercise_2.4.scm
@@ -0,0 +1,13 @@
+#lang sicp
+
+(define (cons x y)
+  (lambda (m) (m x y)))
+
+(define (car z)
+  (z (lambda (p q) p)))
+
+(define (cdr z)
+  (z (lambda (p q) q)))
+
+(car (cons 'a 'b))
+(cdr (cons 'a 'b))
