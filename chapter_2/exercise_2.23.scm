3e83cba5830c85d0db577247d155858784b377a1
rawrex <rawrex@proton.me>
2025-01-29 17:57:22 +0700

    Added a solution for the ex 2.23.

---
+++ chapter_2/exercise_2.23.scm
@@ -0,0 +1,7 @@
+#lang sicp
+
+(define (for-each items function)
+  (cond ((null? items) #t)
+        (else (function (car items)) (for-each (cdr items) function))))
+
+(for-each (list 1 2 3 4 5) (lambda (x) (display x)(newline)))
