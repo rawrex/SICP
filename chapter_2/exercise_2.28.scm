5eec543ba18fbd324ef38449ac87b634af53d1c2
rawrex <rawrex@proton.me>
2025-01-31 13:26:58 +0700

    Added a solution for the ex2.28

---
+++ chapter_2/exercise_2.28.scm
@@ -0,0 +1,11 @@
+(define tree (list 1 2 (list 3 4) (list 5 6) (list (list 7 8) (list 9))))
+
+(define (flat input-tree)
+ (define (iter tree result)
+  (cond ((null? tree) result)
+        ((pair? (car tree)) (iter (cdr tree) (append result (flat (car tree)))))
+        (else (iter (cdr tree) (append result (list (car tree)))))))
+ (iter input-tree '()))
+
+tree
+(flat tree)
